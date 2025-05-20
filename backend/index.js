ce7793283f8820158ba43a9a5b0fad02d2136345
nihal-soni <nihalsoni0523@gmail.com>
2025-05-20 20:07:48 +0530

    feat: implement MongoDB connection and update dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,22 @@
 import express from 'express';
 import { nanoid } from 'nanoid';
+import dotenv from 'dotenv';
+import connectDB from './src/config/mongo.config.js';
+dotenv.config("./.env")
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.post("/api/create", (req, res) => {
     const url = req.body
-    console.log(url);
     res.send(nanoid(7))
 })
 
 app.listen(PORT, () => {
+    connectDB()
     console.log(`Server is running on http://localhost:${PORT}`);
 }
 );
- 
